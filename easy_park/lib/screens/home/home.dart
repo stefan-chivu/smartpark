df082e8db67c7b90cb7f90b6088cb77aebcdf711
Stefan Chivu <stefan.chivu1@yahoo.com>
2022-10-24 21:08:13 +0300

    maps: Added map on home screen
    Added map viewer in Home() with sample location

--- easy_park/lib/screens/home/home.dart
+++ easy_park/lib/screens/home/home.dart
@@ -1,5 +1,8 @@
+import 'dart:async';
+
 import 'package:easy_park/ui_components/custom_app_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -9,9 +12,23 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  Completer<GoogleMapController> _controller = Completer();
+
+  static final CameraPosition _location = CameraPosition(
+    target: LatLng(45.747631, 21.226129),
+    zoom: 19,
+  );
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: const CustomAppBar(showHome: false), body: Text("Hello"));
+        appBar: const CustomAppBar(showHome: false),
+        body: GoogleMap(
+          mapType: MapType.normal,
+          initialCameraPosition: _location,
+          onMapCreated: (GoogleMapController controller) {
+            _controller.complete(controller);
+          },
+        ));
   }
 }
