2ab46778177a5dd87a784afffee1dae833c84cce
Stefan Chivu <schivu@cloudbasesolutions.com>
2022-10-25 19:01:38 +0300

    maps: Added markers for parking spots

--- easy_park/lib/services/sql.dart
+++ easy_park/lib/services/sql.dart
@@ -8,23 +8,25 @@ class SqlService {
     userName: const String.fromEnvironment('EASYPARK_SQL_USERNAME'),
     password: const String.fromEnvironment('EASYPARK_SQL_PASSWORD'),
     maxConnections: 10,
-    databaseName: 'easypark', // optional,
+    databaseName: 'easypark',
+    secure: true,
   );
 
   //TODO: Add more specific parameters
   // i.e. town for narrower query
   Future<List<ParkingInfo>> getParkingSpots() async {
     List<ParkingInfo> parkingInfo = List.empty(growable: true);
-    var result = await pool.execute("SELECT * FROM parking_info");
+    print("Attempting SQL Connection");
+    var result = await pool.execute("SELECT * FROM Sensors");
+    print("Fetched SQL info");
 
     for (var row in result.rows) {
       int id = row.typedColByName<int>("ID")!;
-      bool occupied = row.typedColByName<bool>("STATE")!;
-      double? lat = row.typedColByName<double>("LATITUDE");
-      double? long = row.typedColByName<double>("LONGITUDE");
-      if (lat != null && long != null) {
-        parkingInfo.add(ParkingInfo(id, occupied, lat, long));
-      }
+      bool occupied = row.typedColByName<bool>("OCCUPIED")!;
+      double lat = row.typedColByName<double>("LATITUDE")!;
+      double long = row.typedColByName<double>("LONGITUDE")!;
+
+      parkingInfo.add(ParkingInfo(id, occupied, lat, long));
     }
     return parkingInfo;
   }
