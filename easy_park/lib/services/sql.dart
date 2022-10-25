bac355e2acba9a47902837c2513b54948d5769b2
Stefan Chivu <stefan.chivu1@yahoo.com>
2022-10-26 00:11:44 +0300

    sql: Changed db host to GCP vm

--- easy_park/lib/services/sql.dart
+++ easy_park/lib/services/sql.dart
@@ -3,28 +3,30 @@ import 'package:mysql_client/mysql_client.dart';
 
 class SqlService {
   final pool = MySQLConnectionPool(
-    host: '12.12.12.3',
+    host: '35.211.208.184',
     port: 3306,
     userName: 'flutter',
     password: 'P@ssw0rd!',
     maxConnections: 10,
-    databaseName: 'easypark', // optional,
+    databaseName: 'easypark',
+    secure: true,
   );
 
   //TODO: Add more specific parameters
   // i.e. town for narrower query
   Future<List<ParkingInfo>> getParkingSpots() async {
     List<ParkingInfo> parkingInfo = List.empty(growable: true);
-    var result = await pool.execute("SELECT * FROM parking_info");
+    print("Attempting SQL Connection");
+    var result = await pool.execute("SELECT * FROM Sensors");
+    print("Fetched SQL info");
 
     for (var row in result.rows) {
       int id = row.typedColByName<int>("ID")!;
-      bool occupied = row.typedColByName<bool>("STATE")!;
-      double? lat = row.typedColByName<double>("LATITUDE");
-      double? long = row.typedColByName<double>("LONGITUDE");
-      if (lat != null && long != null) {
-        parkingInfo.add(ParkingInfo(id, occupied, lat, long));
-      }
+      bool occupied = row.typedColByName<bool>("OCCUPIED")!;
+      double lat = row.typedColByName<double>("LATITUDE")!;
+      double long = row.typedColByName<double>("LONGITUDE")!;
+
+      parkingInfo.add(ParkingInfo(id, occupied, lat, long));
     }
     return parkingInfo;
   }
