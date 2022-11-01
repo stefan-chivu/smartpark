aa2362dc5aba39366d8f7b8a00e95e152c651841
Stefan Chivu <stefan.chivu1@yahoo.com>
2022-11-01 23:09:42 +0200

    maps: Added refresh button for spot status

--- easy_park/lib/screens/home/home.dart
+++ easy_park/lib/screens/home/home.dart
@@ -7,6 +7,7 @@ import 'package:easy_park/ui_components/custom_app_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:location/location.dart';
+import 'package:multi_split_view/multi_split_view.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -21,6 +22,7 @@ class _HomeState extends State<Home> {
   Completer<GoogleMapController> _controller = Completer();
   late Future<CameraPosition> _crtPosition;
   late Future<List<ParkingInfo>> _parkingSpots;
+  Set<Marker> _markers = {};
   SqlService sqlService = SqlService();
 
   @override
@@ -38,7 +40,7 @@ class _HomeState extends State<Home> {
     }
 
     return CameraPosition(
-        target: LatLng(data.latitude!, data.longitude!), zoom: 19);
+        target: LatLng(data.latitude!, data.longitude!), zoom: 18);
   }
 
   Future<List<ParkingInfo>> _getParkingSpots() async {
@@ -47,33 +49,48 @@ class _HomeState extends State<Home> {
     return spots;
   }
 
-  Set<Marker> _getMarkers(List<ParkingInfo> spots) {
-    Set<Marker> markers = {};
-    for (ParkingInfo s in spots) {
-      if (s.position != null) {
-        markers.add(Marker(
-          //add first marker
-          markerId: MarkerId(s.id.toString()),
-          position: s.position!, //position of marker
-          infoWindow: InfoWindow(
-            //popup info
-            title: 'Spot #${s.id}',
-            snippet: 'Price: 5RON/h',
-          ),
-          icon: (s.occupied)
-              ? BitmapDescriptor.defaultMarker
-              : BitmapDescriptor.defaultMarkerWithHue(
-                  BitmapDescriptor.hueGreen), //Icon for Marker
-        ));
+  Future<void> _getMarkers(List<ParkingInfo> spots) async {
+    if (_markers.isEmpty) {
+      for (ParkingInfo s in spots) {
+        _markers.add(Marker(
+                //add first marker
+                markerId: MarkerId(s.sensorId.toString()),
+                position: s.position, //position of marker
+                infoWindow: InfoWindow(
+                  //popup info
+                  title: 'Spot #${s.sensorId}',
+                  snippet: 'Price: ${s.zone.hourRate}RON/h',
+                ),
+                // icon: getMarkerIcon()
+                icon: BitmapDescriptor.defaultMarkerWithHue(
+                    BitmapDescriptor.hueRed))
+            //Icon for Marker
+            );
       }
+    } else {
+      Set<Marker> newMarkers = {};
+      for (Marker m in _markers) {
+        bool occupied =
+            await sqlService.getSensorStatus(int.parse(m.markerId.value));
+        newMarkers.add(m.copyWith(iconParam: getMarkerIcon(occupied)));
+      }
+      _markers.removeAll(_markers.toSet());
+      setState(() {
+        _markers.addAll(newMarkers);
+      });
     }
-    return markers;
+  }
+
+  BitmapDescriptor getMarkerIcon(bool occupied) {
+    return occupied
+        ? BitmapDescriptor.defaultMarker
+        : BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueGreen);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: const CustomAppBar(showHome: false),
+        appBar: const CustomAppBar(showHome: true),
         body: FutureBuilder(
           future: Future.wait([_crtPosition, _parkingSpots]),
           builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
@@ -91,17 +108,36 @@ class _HomeState extends State<Home> {
                 );
               default:
                 if (snapshot.hasData) {
-                  print(snapshot.data[0]);
-                  Set<Marker> markers = _getMarkers(snapshot.data[1]);
-                  print(markers);
-                  return GoogleMap(
-                      mapType: MapType.normal,
-                      initialCameraPosition: snapshot.data[0],
-                      myLocationEnabled: true,
-                      markers: markers,
-                      onMapCreated: (GoogleMapController controller) {
-                        _controller.complete(controller);
-                      });
+                  if (_markers.isEmpty) {
+                    _getMarkers(snapshot.data[1]);
+                  }
+                  return MultiSplitView(
+                      axis: Axis.vertical,
+                      resizable: false,
+                      controller:
+                          MultiSplitViewController(areas: [Area(weight: 0.7)]),
+                      children: [
+                        GoogleMap(
+                            mapType: MapType.normal,
+                            initialCameraPosition: snapshot.data[0],
+                            myLocationEnabled: true,
+                            markers: _markers,
+                            onMapCreated: (GoogleMapController controller) {
+                              if (!_controller.isCompleted) {
+                                _controller.complete(controller);
+                              }
+                            }),
+                        Scaffold(
+                          floatingActionButton: FloatingActionButton(
+                            onPressed: () {
+                              _getMarkers(snapshot.data[1]);
+                            },
+                            child: Icon(Icons.refresh),
+                          ),
+                          floatingActionButtonLocation:
+                              FloatingActionButtonLocation.centerFloat,
+                        ),
+                      ]);
                 }
                 if (snapshot.hasError) {
                   return Text('Error: ${snapshot.error}');
