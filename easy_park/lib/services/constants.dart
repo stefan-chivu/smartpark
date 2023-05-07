fe777c4071003d36468056425fb16688fe968d0e
Stefan Chivu <stefan.chivu1@yahoo.com>
2023-05-07 19:49:27 +0300

    Renamed constants that contained EASYPARK to SMARTPARK

--- easy_park/lib/services/constants.dart
+++ easy_park/lib/services/constants.dart
@@ -1,9 +1,9 @@
 abstract class Constants {
-  static const String sqlHost = String.fromEnvironment('EASYPARK_SQL_HOST');
+  static const String sqlHost = String.fromEnvironment('SMARTPARK_SQL_HOST');
   static const String sqlUserName =
-      String.fromEnvironment('EASYPARK_SQL_USERNAME');
+      String.fromEnvironment('SMARTPARK_SQL_USERNAME');
   static const String sqlPassword =
-      String.fromEnvironment('EASYPARK_SQL_PASSWORD');
+      String.fromEnvironment('SMARTPARK_SQL_PASSWORD');
   static const String googleMapsApiKey =
       String.fromEnvironment('GOOGLE_MAPS_API_KEY');
   static final alNumRegExp = RegExp(r'^[a-zA-Z0-9]+$');
