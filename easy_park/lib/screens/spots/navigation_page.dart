51e7a429094613757126b97a72c12db0c33aa9ce
Stefan Chivu <stefan.chivu1@yahoo.com>
2023-05-30 17:04:48 +0300

    spot redirection logic

--- easy_park/lib/screens/spots/navigation_page.dart
+++ easy_park/lib/screens/spots/navigation_page.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+
 import 'package:easy_park/models/parking_info.dart';
+import 'package:easy_park/screens/error.dart';
 import 'package:easy_park/services/sql.dart';
 import 'package:easy_park/ui_components/custom_nav_bar.dart';
 import 'package:easy_park/ui_components/ui_specs.dart';
@@ -31,6 +34,9 @@ class _NavigationPageState extends State<NavigationPage> {
   late Position origin;
   late LatLng destination;
   late SpotInfo spot;
+
+  late Timer timer;
+  bool _timerOpsCompleted = true;
 
   @override
   void initState() {
@@ -151,6 +157,106 @@ class _NavigationPageState extends State<NavigationPage> {
               if (_controller != null) {
                 _controller!.startNavigation(options: _options);
               }
+
+              timer = Timer.periodic(const Duration(seconds: 5), (timer) async {
+                if (_timerOpsCompleted) {
+                  setState(() {
+                    _timerOpsCompleted = false;
+                  });
+                  SpotState status =
+                      await SqlService.getSensorStatus(spot.sensorId);
+
+                  if (status == SpotState.occupied) {
+                    timer.cancel();
+                    SqlService.clearSpotReservation(spot.sensorId);
+
+                    bool found = false;
+                    SpotInfo? newSpot;
+                    // if the sensor that the user was currently going to gets occupied,
+                    // route the user to the nearest sensor within a 330m, 660m, 990m range
+                    for (double searchArea = 0.33;
+                        searchArea < 1 && !found;
+                        searchArea += 0.33) {
+                      newSpot = await SqlService
+                          .getNearestAvailableParkingSpotWithinRange(
+                              destination.latitude,
+                              destination.longitude,
+                              searchArea);
+                      if (newSpot != null) {
+                        break;
+                      }
+                    }
+
+                    if (newSpot == null && mounted) {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: ((context) {
+                        return const ErrorPage(
+                          errorMsg:
+                              "Uh oh! The spot you were going to got occupied and we couldn't find another one within 1km",
+                        );
+                      })));
+                    } else {
+                      await showDialog(
+                          context: context,
+                          builder: (context) {
+                            return AlertDialog(
+                              title: const Text('Parking spot occupied!'),
+                              content: const Text(
+                                  'The spot you were going to got occupied. Luckily, we found another one! Shall we go there?'),
+                              actions: [
+                                // The "Yes" button
+
+                                TextButton(
+                                    onPressed: () async {
+                                      Position crtLocation =
+                                          await Geolocator.getCurrentPosition();
+                                      setState(() {
+                                        destination = LatLng(newSpot!.latitude,
+                                            newSpot.longitude);
+                                        spot = newSpot;
+                                        _controller!.finishNavigation();
+
+                                        setState(() {
+                                          _isNavigating = false;
+                                          origin = crtLocation;
+                                        });
+                                        _controller!.buildRoute(wayPoints: [
+                                          WayPoint(
+                                              name: 'Origin',
+                                              latitude: origin.latitude,
+                                              longitude: origin.longitude),
+                                          WayPoint(
+                                              name: 'Destination',
+                                              latitude: destination.latitude,
+                                              longitude: destination.longitude)
+                                        ]);
+                                      });
+                                      await SqlService.reserveSpot(
+                                          spot.sensorId);
+
+                                      // Close the dialog
+                                      Navigator.of(context).pop();
+                                    },
+                                    child: const Text('Yes')),
+                                TextButton(
+                                    onPressed: () {
+                                      Navigator.pushNamedAndRemoveUntil(
+                                          context, '/', (route) => false);
+                                    },
+                                    child: const Text('No'))
+                              ],
+                            );
+                          });
+                    }
+                  }
+
+                  setState(() {
+                    _timerOpsCompleted = true;
+                  });
+                } else {
+                  print("Timer not completed");
+                }
+              });
             },
             style: ElevatedButton.styleFrom(
                 backgroundColor: AppColors.emerald,
@@ -308,18 +414,21 @@ class _NavigationPageState extends State<NavigationPage> {
         });
         break;
       case MapBoxEvent.navigation_running:
+        print("Navigation running event received");
         setState(() {
           _isNavigating = true;
         });
         break;
       case MapBoxEvent.on_arrival:
         if (!_isMultipleStop) {
+          // TODO: show arrival message
           await Future.delayed(const Duration(seconds: 3));
           await _controller!.finishNavigation();
         } else {}
         break;
       case MapBoxEvent.navigation_finished:
       case MapBoxEvent.navigation_cancelled:
+        print("Navigation stopped event received");
         setState(() {
           _routeBuilt = false;
           _isNavigating = false;
