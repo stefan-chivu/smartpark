d94dd87cec9aa4d9c9730513783ae0f3e3dba6b4
Stefan Chivu <stefan.chivu1@yahoo.com>
2023-06-08 18:02:55 +0300

    Display nearest spot after searching an address on homepage

--- easy_park/lib/screens/home/home.dart
+++ easy_park/lib/screens/home/home.dart
@@ -1,16 +1,19 @@
 import 'package:easy_park/models/address.dart';
+import 'package:easy_park/models/spot_info.dart';
 import 'package:easy_park/providers/parking_spot_provider.dart';
 import 'package:easy_park/screens/error.dart';
 import 'package:easy_park/services/constants.dart';
 import 'package:easy_park/services/location.dart';
+import 'package:easy_park/services/places.dart';
+import 'package:easy_park/services/sql.dart';
 import 'package:easy_park/ui_components/custom_nav_bar.dart';
 import 'package:easy_park/ui_components/loading_snack_bar.dart';
-import 'package:easy_park/ui_components/search_address_textfield.dart';
 import 'package:easy_park/ui_components/ui_specs.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:uuid/uuid.dart';
 
 class Home extends ConsumerStatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -27,6 +30,7 @@ class _HomeState extends ConsumerState<Home> {
   LatLng? tmpPosition;
   final TextEditingController _controller = TextEditingController();
   GoogleMapController? _mapController;
+  bool _enableMapGestures = true;
 
   @override
   void dispose() {
@@ -73,6 +77,9 @@ class _HomeState extends ConsumerState<Home> {
               FloatingActionButtonLocation.miniCenterFloat,
           body: Stack(alignment: AlignmentDirectional.topStart, children: [
             GoogleMap(
+              scrollGesturesEnabled: _enableMapGestures,
+              zoomGesturesEnabled: _enableMapGestures,
+              tiltGesturesEnabled: false,
               myLocationButtonEnabled: false,
               mapToolbarEnabled: false,
               mapType: MapType.normal,
@@ -85,8 +92,11 @@ class _HomeState extends ConsumerState<Home> {
                 _mapController!.setMapStyle(Constants.mapStyleString);
               },
               onCameraMoveStarted: () {
-                showRefresh = true;
-                setState(() {});
+                if (_enableMapGestures) {
+                  setState(() {
+                    showRefresh = true;
+                  });
+                }
               },
               onCameraMove: (position) {
                 setState(() {
@@ -99,10 +109,106 @@ class _HomeState extends ConsumerState<Home> {
             Padding(
                 padding: const EdgeInsets.symmetric(
                     horizontal: AppMargins.S, vertical: AppMargins.L),
-                child: SearchAddressTextField(
-                  label: 'Search...',
+                child: TextField(
                   controller: _controller,
-                  mapController: _mapController,
+                  readOnly: true,
+                  decoration: InputDecoration(
+                    fillColor: Colors.white,
+                    filled: true,
+                    labelText: "Search...",
+                    border: OutlineInputBorder(
+                      borderSide: const BorderSide(width: 1), //<-- SEE HERE
+                      borderRadius: BorderRadius.circular(20.0),
+                    ),
+                  ),
+                  onTap: () async {
+                    final sessionToken = const Uuid().v4();
+                    final Suggestion? result = await showSearch(
+                      context: context,
+                      delegate: AddressSearch(sessionToken: sessionToken),
+                    );
+
+                    if (result != null && result.description.isNotEmpty) {
+                      bool found = false;
+                      SpotInfo? closestSpot;
+                      // route the user to the nearest sensor within a 330m, 660m, 990m range
+                      for (double searchArea = 0.33;
+                          searchArea < 1 && !found;
+                          searchArea += 0.33) {
+                        closestSpot = await SqlService
+                            .getNearestAvailableParkingSpotWithinRange(
+                                result.location!.latitude,
+                                result.location!.longitude,
+                                searchArea);
+                        if (closestSpot != null) {
+                          break;
+                        }
+                      }
+
+                      if (_mapController != null && result.location != null) {
+                        setState(() {
+                          _controller.text = result.description;
+                          _mapController!.animateCamera(
+                              CameraUpdate.newLatLngZoom(
+                                  result.location!, 16.5));
+                        });
+                        if (closestSpot != null) {
+                          setState(() {
+                            _enableMapGestures = false;
+                            showRefresh = false;
+                          });
+
+                          if (mounted) {
+                            ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                                backgroundColor: AppColors.blueGreen,
+                                content: Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: const [
+                                    Text(
+                                      'Getting you to the nearest available spot',
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: AppFontSizes.M),
+                                    )
+                                  ],
+                                )));
+                          }
+                          providerInput.position = LatLng(
+                              closestSpot.latitude, closestSpot.longitude);
+                          ref.refresh(spotProvider(providerInput));
+                          await Future.delayed(const Duration(seconds: 5));
+
+                          setState(() {
+                            providerInput.position = result.location!;
+                            _mapController!.animateCamera(
+                                CameraUpdate.newLatLng(LatLng(
+                                    closestSpot!.latitude,
+                                    closestSpot.longitude)));
+                            _mapController!.showMarkerInfoWindow(
+                                MarkerId(closestSpot.sensorId.toString()));
+                            _enableMapGestures = true;
+                          });
+                        } else {
+                          if (mounted) {
+                            ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                                backgroundColor: AppColors.sandyBrown,
+                                content: Wrap(
+                                  alignment: WrapAlignment.center,
+                                  children: const [
+                                    Text(
+                                      'No spots were found within 1km of the address you entered',
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          color: Colors.black,
+                                          fontSize: AppFontSizes.M),
+                                    )
+                                  ],
+                                )));
+                          }
+                        }
+                      }
+                    }
+                  },
                 )),
             Align(
               alignment: Alignment.bottomLeft,
